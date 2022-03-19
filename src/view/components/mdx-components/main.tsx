c8da236a4ff000ba5d2dca9bda514b49c4f54b63
waieneiaw <15177530+waieneiaw@users.noreply.github.com>
2022-03-19 09:59:03 +0000

    add `a` tag

--- src/view/components/mdx-components/main.tsx
+++ src/view/components/mdx-components/main.tsx
@@ -1,5 +1,29 @@
 /* eslint-disable @typescript-eslint/naming-convention */
 
+import React from 'react';
+
+const A = ({
+  href,
+  children,
+  target,
+  rel,
+  ...props
+}: React.AnchorHTMLAttributes<HTMLAnchorElement>) => {
+  if (href?.startsWith('http')) {
+    return (
+      <a href={href} target="_blank" rel="noopener noreferrer" {...props}>
+        {children}
+      </a>
+    );
+  }
+
+  return (
+    <a href={href} target={target} rel={rel} {...props}>
+      {children}
+    </a>
+  );
+};
+
 const Img = ({
   src,
   alt,
@@ -9,5 +33,6 @@ const Img = ({
 );
 
 export const MDXComponents = {
+  a: A,
   img: Img,
 };
